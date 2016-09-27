86264f484792cb3a8ab61878c8940d8641b54c3d
Mydel-Ar Asturiano <masturiano@masturiano-lion.perfectfitgroup.local>
2016-09-27 04:14:11 -0400

    2016-09-27

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+# Set default time zone
 date_default_timezone_set('Asia/Manila'); // CDT
+# Format file size
 function formatSizeUnits($bytes)
 {
     if ($bytes >= 1073741824)
@@ -28,15 +30,30 @@ function formatSizeUnits($bytes)
     }
     return $bytes;
 }
-//$directory="/var/www/html/Feeds_File/sample_feeds/";  // local directory
-if($ssh = ssh2_connect('174.129.233.96', 22)) {
+# Start ssh2 connection to server
+if($ssh = ssh2_connect('174.129.233.96', 22)) 
+{
+	# Start ssh2 connection credentials
     if(ssh2_auth_password($ssh, 'masturiano', 'masturianousap1q2w')) 
     {
+    	# ssh2 sftp connection
     	$sftp = ssh2_sftp($ssh);
-		//if ($dirhandler = opendir($directory)) // local directory
-		if ($dirhandler = opendir("ssh2.sftp://$sftp/data/aopi/feeds_longbow/output/dam_ci_details/latest/")) 
+    	# account folder location
+    	$dam_ci_details_folder_location = 'data/aopi/feeds_longbow/output/dam_ci_details/latest';
+    	# open account folder location
+		if ($dir_handler = opendir("ssh2.sftp://$sftp/$dam_ci_details_folder_location/")) 
 		{
-			while ($file = readdir($dirhandler)) 
+			?>
+				<table border=1>
+					<tr>
+						<td><b>FILENAME</b></td>
+						<td><b>SIZE</b></td>
+						<td><b>DATE CREATED</b></td>
+						<td><b>TIME CREATED</b></td>
+						<td><b>RECORD COUNT</b></td>
+					</tr>
+			<?php
+			while ($file = readdir($dir_handler)) 
 			{
 				$file_explode_by_dot = explode('.',$file);
 				$file_num_count = (count($file_explode_by_dot)-1);
@@ -45,80 +62,71 @@ if($ssh = ssh2_connect('174.129.233.96', 22)) {
 			    $file_extension = strtolower($file_extension);
 			    if($file_extension == "txt")
 			    {
-			    	//echo $fileSize = filesize("ssh2.sftp://$sftp/$file");
-			    	echo $files[] = $file; // put in array.
-			  		echo $file_size = filesize($file);
-					$file_size_format = formatSizeUnits($file_size);
-					echo $file_size_format;
+			    	?>
+			    	<tr>
+			    	<?php
+			    	#file name
+			    	$filename = $file;
+			    	?>
+			    		<td><?=$filename;?></td>
+			    	<?php
+			    	# file info
+			    	$file_info = "ssh2.sftp://$sftp/$dam_ci_details_folder_location/$file";
+			    	# file size
+			    	$file_size = filesize($file_info);
+			    	# file created
+			    	$file_created = filemtime($file_info);
+			    	# file date created
+			    	$file_date_created = date('F d, Y',($file_created));
+			    	# file time created
+			    	$file_time_created = date('h:i:s A',($file_created));
+			    	# file line count
+			    	$file_line_count = 0;
+					$file_handler = fopen($file_info, "r");
+					while(!feof($file_handler))
+					{
+						$line = fgets($file_handler);
+						$file_line_count++;
+					}
+					fclose($file_handler);
+					if($file_size == '0 bytes')
+					{
+						$file_line_count = 0;
+					}
+					else
+					{
+						$file_line_count = (int)$file_line_count - 1;
+					}
+
+			    	?>
+			    		<td><?=formatSizeUnits($file_size);?></td>
+			    		<td><?=$file_date_created;?></td>
+			    		<td><?=$file_time_created;?></td>
+			    		<td><?=$file_line_count;?></td>
+			    	<?php
+					?>
+			    	</tr>
+			    	<?php
 				}
 			}
-			natsort($files); // sort by filename
 			?>
-			<table border=1>
-				<tr>
-					<td><b>FILENAME</b></td>
-					<td><b>SIZE</b></td>
-					<td><b>DATE CREATED</b></td>
-					<td><b>TIME CREATED</b></td>
-					<td><b>RECORD COUNT</b></td>
-				</tr>
-			<?php
-			foreach($files as $file)
-			{
-				$file_name = $file;
-				$location_file_name = $directory.$file;
-				?>
-				<tr>
-					<td>
-						<?php echo $file_name;?>					
-					</td>
-					<td>
-						<?php 
-							$file_size = filesize($location_file_name);
-							$file_size_format = formatSizeUnits($file_size);
-							echo $file_size_format;
-						?>					
-					</td>
-					<td>
-						<?php 
-							$file_created_date = filectime($location_file_name);
-							echo $file_created = date('F d, Y',$file_created_date);
-						?>					
-					</td>
-					<td align='center'>
-						<?php 
-							$file_created_time = filemtime($location_file_name);
-							echo $file_created = date('h:i:s a',$file_created_time);
-						?>					
-					</td>
-					<td align='right'>
-						<?php 
-							$file_line_count = 0;
-							$handle = fopen($location_file_name, "r");
-							while(!feof($handle))
-							{
-								$line = fgets($handle);
-								$file_line_count++;
-							}
-							fclose($handle);
-							if($file_size_format == '0 bytes')
-							{
-								echo 0;
-							}
-							else
-							{
-								echo $file_line_count;
-							}
-							
-						?>					
-					</td>
-				</tr>
-				<?php
-			}
-			?>
-			</table>
+				</table>
 			<?php
 		}
+		else
+		{
+			echo "Failed to open the directory.";
+    		echo "<br/>";
+		}
+	}
+	else
+	{
+		echo "Failed to connect using the account.";
 	}
 }
+else
+{
+	echo "Failed to connect to remote server.";
+}
+
 ?>
